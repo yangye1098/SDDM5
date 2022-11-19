bfa0597a246a8e7aec80186c75ab4692e888547b
Ye <yangye1098@gmail.com>
2022-11-18 21:35:33 -0800

    implemenent ncspp_rep

--- model/network.py
+++ model/network.py
@@ -2,4 +2,5 @@ from .diffwave import DiffWave
 from .diffwave2 import DiffWave2
 from .unetwithspec import UNetWithSpec
 from .unet import UNet
+from .ncsnpp_rep import NCSNPP_REP
 from .wavegrad import DenoiseWaveGrad
